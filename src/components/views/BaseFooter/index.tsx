c3843e4a5883b04547004a7f6f8b8e3b177da2e8
NamS135176 <namnt1706@gmail.com>
2020-04-14 13:16:21 +0700

    make: header and footer

--- src/components/views/BaseFooter/index.tsx
+++ src/components/views/BaseFooter/index.tsx
@@ -1,11 +1,52 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "./styles.scss";
+// import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faPinterestP, faGooglePlus, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+function Brand(props: any) {
+  return (
+    <div className="ml-4">
+        <a href={props.linkTo} >
+          <FontAwesomeIcon icon={props.brand} />
+        </a>
+    </div>
+  )
+}
+
+function Contact(props: any) {
+  return (
+    <div className="ml-3">
+        <a href={props.linkTo} >
+        {props.contact}
+        </a>
+    </div>
+  )
+}
 
 const BaseHeader: React.FC = () => (
   <footer className="footer">
-    <Container className="text-center text-white">
-      Footer
+    <Container className="text-center text-white p-4 d-flex justify-content-between">
+      <div className="col-4 d-flex justify-content-start ">
+        <span>
+          @2019 Logwork. All right reserved
+        </span>
+      </div>
+      <div className="col-4 d-flex justify-content-center">
+        <Contact linkTo={"#"} contact="Home" />
+        <Contact linkTo={"#"} contact="Recipes" />
+        <Contact linkTo={"#"} contact="Article" />
+        <Contact linkTo={"#"} contact="Contact" />
+        <Contact linkTo={"#"} contact="Purchase" />
+      </div>
+      <div className="col-4 d-flex justify-content-end brands" >
+        <Brand linkTo={"#"} brand={faFacebook} />
+        <Brand linkTo={"#"} brand={faTwitter} />
+        <Brand linkTo={"#"} brand={faGooglePlus} />
+        <Brand linkTo={"#"} brand={faPinterestP} />
+        <Brand linkTo={"#"} brand={faInstagram} />
+      </div>
     </Container>
   </footer>
 )
